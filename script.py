e9f2ccc055175467db979caee9427d868dcd17b5
andrewgr1234 <85031521+andrewgr1234@users.noreply.github.com>
2022-06-23 00:53:19 +0300

    Update script.py

--- script.py
+++ script.py
@@ -3,8 +3,10 @@ text= """
 """
 lst = text.splitlines()
 print(lst)
-mylist = [" ", " " ]
+mylist = [" ", " "]
 for index in mylist:
     for line in lst:
+        if not index in text:
+            print('The Word' , index ,'Was Not Found')
         if index in line:
             print('The Word :'  , index , "- The Line : " , lst.index(line)+1)
